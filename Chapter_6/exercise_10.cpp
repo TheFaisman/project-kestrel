02c0f9331ba8710ebe212c8f1bad7854ca96b7c9
Faisal Naqaweh <f.naqaweh@gmail.com>
2023-12-28 23:41:02 -0500

    boiler plate for exercise 10

---
+++ Chapter_6/exercise_10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/*
+    A permutation is an ordered subset of a set. For example, say you wanted to pick a combination
+    to a vault. There are 60 possible numbers, and you need three different numbers for the combination.
+    There are P(60,3) permutations for the combinations, where P is defined by 
+    
+        P(a,b) = a! / (a-b)!
+
+    Combinations are similar to permutations, except that the order doesn't matter. The formula is
+
+        C(a,b) = P(a,b) / b!
+
+    Design a program that asks users for two numbers, and asks them whether they want to calculate
+    permutations or combinations, and prints the result.
+*/
+
+int main() {
+    return 0;
+}
